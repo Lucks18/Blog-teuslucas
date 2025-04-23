2ab1b7512a751f28ebe6591ad61d4baf9aecab85
Lucks18 <108702721+Lucks18@users.noreply.github.com>
2025-04-23 02:53:42 -0300

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -70,7 +70,7 @@ function cabecalho(cabecalhos) {
                 //document.write('verdadeiro');
             } if(window.location.pathname === '/' || window.location.pathname === '/Blog-teuslucas/' || window.location.pathname.endsWith('/index.html')) {
                 //a3.href = 'Paginas/sobre.html';
-                a3.href = '/Blog-teuslucas/Paginas/sobre.html';
+                //a3.href = '/Blog-teuslucas/Paginas/sobre.html';
             }
         }
 
@@ -83,7 +83,8 @@ function cabecalho(cabecalhos) {
                 //document.write('verdadeiro');
             } if(window.location.pathname === '/' || window.location.pathname === '/Blog-teuslucas/' || window.location.pathname.endsWith('/index.html')) {
                 //a7.href = 'Paginas/contato.html';
-                a7.href = '#';
+                a7.href = '/Blog-teuslucas/Paginas/contato.html';
+                document.write('verdadeiro');
             }
         }
 
